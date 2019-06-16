21cee679bda291fde418b0bc83a8e14816c1c902
Joel Tengco <joeltengco@yahoo.com>
2019-06-15 20:17:30 -0700

    implement navbar at top of pages

---
+++ src/scripts/navbar.js
@@ -0,0 +1,33 @@
+var navbar = $('<div></div>').attr({ id: 'navbar' })
+var infoLink = $('<a>INFO</a>').attr({
+  id: 'infoLink',
+  href: './info.html'
+})
+var resLink = $('<a>RESOURCES</a>').attr({
+  id: 'resLink',
+  href: './res.html'
+})
+var timelineLink = $('<a>TIMELINE</a>').attr({
+  id: 'timelineLink',
+  href: './timeline.html'
+})
+var todoLink = $('<a>TASKS</a>').attr({
+  id: 'tasksLink',
+  href: './tasks.html'
+})
+var issuesLink = $('<a>ISSUES</a>').attr({
+  id: 'issuesLink',
+  href: './issues.html'
+})
+var archiveLink = $('<a>ARCHIVE</a>').attr({
+  id: 'archiveLink',
+  href: './archive.html'
+})
+
+navbar.append(
+  infoLink, resLink, timelineLink,
+  todoLink, issuesLink, archiveLink
+)
+$(document).ready(() => {
+  $(document.body).prepend(navbar)
+})
