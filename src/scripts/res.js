1e44352ca768cd5d8e63747e970a830f39030745
Joel Tengco <joeltengco@yahoo.com>
2019-06-19 16:23:41 -0700

    implement adding resources with dialog.showOpenDialog(); change add button in tasks page to have id addBtn instead of class

--- src/scripts/res.js
+++ src/scripts/res.js
@@ -1,24 +1,51 @@
-const fs = require('fs')
+const allowSaving = eval(sessionStorage.allowSaving)
+
+const remote = require('electron').remote
+const dialog = remote.dialog
 
 let projectData = JSON.parse(sessionStorage.projectData)
-const resDir = sessionStorage.projDir + 'res/'
 const resources = projectData.resources
 
-if (resources.length > 0) {
-  // remove empty list text and append all the resources
-  $('#list').empty()
-  resources.forEach((res) => {
-    var container = $('<div></div>').attr({
-      class: 'res',
-      id: res.title
-    })
-    var img = $('<img>').attr({ src: resDir + res.ref })
-    var title = $('<h2></h2>').text(res.title)
-    var desc = $('<p></p>').text(res.desc)
-    container.append(img, $('<div></div>').append(title, desc))
-    $('#list').append(container)
+function addResource(res) {
+  var container = $('<div></div>').attr({
+    class: 'resDiv',
+    id: 'res-' + res.hashid
   })
+  var img = $('<img>').attr({ src: res.ref })
+  var title = $('<h2></h2>').text(res.title)
+  var desc = $('<p></p>').text(res.desc)
+  container.append(img, $('<div class="resInfo"></div>').append(title, desc))
+  $('#list').append(container)
 }
+
+resources.forEach((res) => { addResource(res) })
+
+var addBtn = $('<button>Add resources</button>').on('click', () => {
+  var results = dialog.showOpenDialog(remote.getCurrentWindow(), {
+    title: 'Add resources',
+    filters: [
+      { name: 'Images', extensions: ['png', 'jpg', 'gif'] }
+    ],
+    properties: ['openFile', 'multiSelections']
+  })
+
+  results.forEach((path) => {
+    let res = {
+      hashid: new Date().getTime()
+        ^ Math.floor(Math.random() * Number.MAX_SAFE_INTEGER),
+      title: 'Untitled',
+      desc: 'empty description',
+      ref: path
+    }
+    addResource(res)
+    resources.push(res)
+  })
+  sessionStorage.projectData = JSON.stringify(projectData)
+  allowSaving()
+}) // end add button on click func
+addBtn.attr('id', 'addBtn')
+
+$('#content').append(addBtn)
 
 $(() => {
   $('#resLink').addClass('activeLink')
--- src/scripts/tasks.js
+++ src/scripts/tasks.js
@@ -89,7 +89,7 @@ function addTask(task) {
 projectTasks.forEach((task) => { addTask(task) })
 
 // button to add a new task to the table
-$('#content').append($('<button class="addBtn">+</button>').on('click', () => {
+$('#content').append($('<button id="addBtn">+</button>').on('click', () => {
   var task = {
     hashid: new Date().getTime()
       ^ Math.floor(Math.random() * Number.MAX_SAFE_INTEGER),
