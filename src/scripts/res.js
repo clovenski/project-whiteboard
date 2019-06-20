f38b2f48a85497149d855658c7e745168623a7d3
Joel Tengco <joeltengco@yahoo.com>
2019-06-19 18:01:25 -0700

    implement error event handler when loading resource images; style resource info division

--- src/scripts/res.js
+++ src/scripts/res.js
@@ -11,9 +11,37 @@ function addResource(res) {
     class: 'resDiv',
     id: 'res-' + res.hashid
   })
-  var img = $('<img>').attr({ src: res.ref })
-  var title = $('<h2></h2>').text(res.title)
-  var desc = $('<p></p>').text(res.desc)
+  var img = $('<img>').attr({ src: res.ref, class: 'resImg' })
+  img.on('error', () => {
+    let sub = $('<div>File not found. Click to add reference.</div>')
+    sub.attr({ class: 'placeholder' })
+    sub.on('click', () => {
+      let result = dialog.showOpenDialog(remote.getCurrentWindow(), {
+        title: 'Add resources',
+        filters: [
+          { name: 'Images', extensions: ['png', 'jpg', 'gif'] }
+        ],
+        properties: ['openFile']
+      })
+      if (result !== undefined) {
+        container.children('.placeholder').remove()
+        container.prepend($('<img>').attr({ src: result[0], class: 'resImg' }))
+        resources.some((elem, i) => {
+          if (elem.hashid == res.hashid) {
+            resources[i].ref = result[0]
+            return true
+          }
+        })
+        sessionStorage.projectData = JSON.stringify(projectData)
+        allowSaving()
+      }
+    }) // end substitute onclick func
+    container.children('.resImg').remove()
+    container.prepend(sub)
+  }) // end error handling when broken ref
+
+  var title = $('<h2 class="resTitle"></h2>').text(res.title)
+  var desc = $('<p class="resDesc"></p>').text(res.desc)
   container.append(img, $('<div class="resInfo"></div>').append(title, desc))
   $('#list').append(container)
 }
@@ -29,19 +57,21 @@ var addBtn = $('<button>Add resources</button>').on('click', () => {
     properties: ['openFile', 'multiSelections']
   })
 
-  results.forEach((path) => {
-    let res = {
-      hashid: new Date().getTime()
-        ^ Math.floor(Math.random() * Number.MAX_SAFE_INTEGER),
-      title: 'Untitled',
-      desc: 'empty description',
-      ref: path
-    }
-    addResource(res)
-    resources.push(res)
-  })
-  sessionStorage.projectData = JSON.stringify(projectData)
-  allowSaving()
+  if (results !== undefined) {
+    results.forEach((path) => {
+      let res = {
+        hashid: new Date().getTime()
+          ^ Math.floor(Math.random() * Number.MAX_SAFE_INTEGER),
+        title: 'Untitled',
+        desc: 'empty description',
+        ref: path
+      }
+      addResource(res)
+      resources.push(res)
+    })
+    sessionStorage.projectData = JSON.stringify(projectData)
+    allowSaving()
+  }
 }) // end add button on click func
 addBtn.attr('id', 'addBtn')
 
